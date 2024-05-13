d20bce2f0a38ae900eff7e998c635d1631854413
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-05-13 19:19:34 +0900

    docs : 0513 props 연습 날씨 추가하기

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -3,13 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Todolist from "./Todolist";
 import Clock from "./timer";
-
+import MyWeather from "./MyWeather";
 function App() {
   return (
     <div className="container">
       <Todolist></Todolist>
       <br />
-      <Clock></Clock>
+      {/* <Clock></Clock> */}
+      <MyWeather weather="맑음">일기예보</MyWeather>
       <br />
 
     </div>
