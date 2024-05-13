1a7ccd5b3671653e539f84a021fd49a57ba8af64
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-05-13 16:14:34 +0900

    Initial commit

---
+++ todolist/src/ClassCom.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ClassCom extends Component{
+    render(){
+        return(
+            <div>
+                클래스형 컴포넌트
+            </div>
+        )
+    }
+}
+
+export default ClassCom;  //외부에서 사용하기 위해 export
